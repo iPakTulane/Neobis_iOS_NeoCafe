fbafc78af20945121ada59a141be8d2711364c41
Igor Pak <ipak.tulane@gmail.com>
2024-01-24 11:32:58 +0500

    Add README file

---
+++ Neobis_iOS_NeoCafe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Neobis_iOS_NeoCafe
+//
+//  Created by iPak Tulane on 24/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
